c88d4ad800f511e4fd9c56d71a87d369ed933c12
Sathmi1234 <devsadini@gmail.com>
2024-12-24 09:47:45 +0530

    created the main layout

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  View } from 'react-native';
+import { Button, TextInput, Text } from 'react-native-paper';
+import { Picker } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.header}>
+        <Text variant="displayLarge">Currency Converter</Text>
+      </View>
+      <Text >Amount:</Text>
+      <TextInput
+        placeholder="Enter amount"
+        keyboardType="numeric"
+      />
+      
+      <Text >From Currency:</Text>
+      <Picker>
+        <Picker.Item label="USD" value="USD" />
+        <Picker.Item label="LKR" value="LKR" />
+        <Picker.Item label="EUR" value="EUR" />
+      </Picker>
+      
+      <Text>To Currency:</Text>
+      <Picker>
+        <Picker.Item label="USD" value="USD" />
+        <Picker.Item label="LKR" value="LKR" />
+        <Picker.Item label="EUR" value="EUR" />
+      </Picker>
+      
+      <Button title="Convert"/>
+     </View>
   );
 }
 
@@ -17,4 +41,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    alignItems:'center'
+  }
 });
